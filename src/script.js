ee0d6bb568581813e2b6bf47cb0bd47a718478d5
Aleksandr Golubovskiy <golubovskiy.al@mail.ru>
2022-05-05 23:46:58 +0300

    feat: added keyup and keydown functions

--- src/script.js
+++ src/script.js
@@ -1,1 +1,67 @@
 import './styles/styles.css';
+
+const keys = document.querySelectorAll('.keys');
+const spaceKey = document.querySelector('.space_key');
+const shiftLeft = document.querySelector('.shift_left');
+const shiftRight = document.querySelector('.shift_right');
+const capsLockKey = document.querySelector('.space_key');
+const toggle_circle = document.querySelector('.toggle__circle');
+const night_mode = document.querySelector('.night__mode');
+const body = document.querySelector('body');
+const text_input = document.querySelector('textarea');
+const change_color = document.querySelector('.change__light__colors');
+const colors_input = document.querySelector('colors__input');
+const keyboard_lights = document.querySelector('keyboard__lights');
+
+for (let i = 0; i < keys.length; i += 1) {
+  keys[i].setAttribute('keyname', keys[i].innerText);
+  keys[i].setAttribute('lowerCaseName', keys[i].innerText.toLowerCase());
+}
+
+window.addEventListener('keydown', (e) => {
+  for (let i = 0; i < keys.length; i += 1) {
+    if (e.key === keys[i].getAttribute('keyname') || e.key === keys[i].getAttribute('lowerCaseName')) {
+      keys[i].classList.add('active');
+    }
+    if (e.code === 'Space') {
+      spaceKey.classList.add('active');
+    }
+    if (e.code === 'ShiftLeft') {
+      shiftRight.classList.remove('active');
+    }
+    if (e.code === 'ShiftRight') {
+      shiftLeft.classList.remove('active');
+    }
+    if (e.code === 'CapsLock') {
+      capsLockKey.classList.toggle('active');
+    }
+  }
+});
+
+window.addEventListener('keyup', (e) => {
+  for (let i = 0; i < keys.length; i += 1) {
+    if (e.key === keys[i].getAttribute('keyname') || e.key === keys[i].getAttribute('lowerCaseName')) {
+      keys[i].classList.remove('active');
+      keys[i].classList.add('remove');
+    }
+
+    if (e.code === 'Space') {
+      spaceKey.classList.remove('active');
+      spaceKey.classList.add('remove');
+    }
+
+    if (e.code === 'ShiftLeft') {
+      shiftRight.classList.remove('active');
+      shiftRight.classList.remove('remove');
+    }
+
+    if (e.code === 'ShiftRight') {
+      shiftLeft.classList.remove('active');
+      shiftLeft.classList.remove('remove');
+    }
+    // eslint-disable-next-line no-loop-func
+    setTimeout(() => {
+      keys[i].classList.remove('remove');
+    }, 200);
+  }
+});
